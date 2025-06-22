72156f1e779c5440b95b9cf0e126e11e8fe16920
Iain-S <25081046+Iain-S@users.noreply.github.com>
2025-06-22 12:26:03 +0100

    Recursion

--- myfunctions.hs
+++ myfunctions.hs
@@ -1,3 +1,5 @@
+import Distribution.Simple.Utils (xargs)
+
 doubleMe x = x + x
 
 doubleSmallNum x = if x > 100 then x else x * 2
@@ -23,20 +25,93 @@ rightTriangles =
   ]
 
 -- Explicit type hints.
-removeNonUppercase :: [Char] -> [Char]  
-removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]  
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [c | c <- st, c `elem` ['A' .. 'Z']]
 
 ident :: a -> a
 ident x = x
 
 -- Constrained with a "class constraint"
-ident' :: Num a => a -> a
+ident' :: (Num a) => a -> a
 ident' x = x
 
 -- Guess the type of head
 -- takes a list of things and returns a thing
-head' :: [a] -> a
-head' xs = head xs
+-- Commented out to silence the warning.
+-- head' :: [a] -> a
+-- head' xs = head xs
 
 -- The type of the == operators is
 -- (==) :: Eq a => a -> a -> Bool
+
+-- pattern matching
+-- note that the order matters
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY SEVEN"
+lucky x = "UNLUCKY"
+
+-- like fst, but for triples
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+headd :: [a] -> a
+headd [a] = a
+headd [] = error "nope"
+headd (x : _) = x
+
+-- the above were patterns, these are guards
+max' a b
+  | a < b = b
+  | otherwise = a
+
+-- Note the indentation of the air and water
+densityTell mazz volume
+  | density < air = "Float"
+  | density <= water = "Swimming"
+  | otherwise = "Sinking!"
+  where
+    density = mazz / volume
+    air = 1.2
+    water = 1000.0
+
+a = densityTell 1.0 2.0
+
+-- This is quite neat!
+calcDensities xs = [density m v | (m, v) <- xs]
+  where
+    density mass volume = mass / volume
+
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * (r ^ 2)
+   in sideArea + 2 * topArea
+
+calcDensities' xs = [m / v | (m, v) <- xs]
+
+calcDensitoy' xs = [density | (m, v) <- xs, let density = m / v, density < 1.2]
+
+calcDensity xs = [density | (m, v) <- xs, let density = m / v, density < 1.2]
+
+-- Case expressions
+describeList :: [a] -> String
+describeList xs =
+  "The list is " ++ case xs of
+    [] -> "empty"
+    [_] -> "singleton"
+    _ -> "two elements or more"
+
+myMaximum :: (Ord a) => [a] -> a
+myMaximum [a] = a
+myMaximum (x : xs)
+  | x > rest = x
+  | otherwise = rest
+  where
+    rest = myMaximum xs
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [x] = x
+maximum' (x : xs)
+  | x > maxTail = x
+  | otherwise = maxTail
+  where
+    maxTail = maximum' xs
